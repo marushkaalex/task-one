d3c8dc9e2fa12bce7426179e77889f17a7d8d88d
Alexander Marushka <marushkaalex@gmail.com>
2016-07-17 11:05:40 +0600

    Add equals() and hashCode to BaseModel

--- src/main/java/com/epam/am/coffee/model/BaseModel.java
+++ src/main/java/com/epam/am/coffee/model/BaseModel.java
@@ -10,4 +10,20 @@ public class BaseModel {
     public void setId(Long id) {
         this.id = id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseModel)) return false;
+
+        BaseModel baseModel = (BaseModel) o;
+
+        return id != null ? id.equals(baseModel.id) : baseModel.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
